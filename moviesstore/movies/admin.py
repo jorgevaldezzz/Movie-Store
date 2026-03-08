3fda7e2ec96f9b364d2434a8626b13c1157124a1
vanordenabdillah <vanordenabdillah@gmail.com>
2026-03-07 21:57:39 -0500

    added movies leaderboard (user story 5)

--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Movie, Review
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.urls import path
 from django.shortcuts import render
 
@@ -35,18 +35,38 @@ class TopCommenterAdmin:
         }
         return render(request, 'admin/top_commenter.html', context)
     
+class TopMovieAdmin:
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('top-movie/', self.admin_view(self.top_movie_view), name='top_movie'),
+        ]
+        return custom_urls + urls
 
+    def top_movie_view(self, request):
+        leaderboard = (Movie.objects.annotate(
+            purchases=Sum('item__quantity')).values('name','purchases').order_by('-purchases'))
+
+        context = {
+            **self.each_context(request),
+            'title': 'Top Movie',
+            'top_movie': leaderboard.first(),
+            'leaderboard': list(leaderboard),
+        }
+        return render(request, 'admin/top_movie.html', context)
 from django.contrib.admin import AdminSite
 original_get_urls = AdminSite.get_urls
 
 def patched_get_urls(self):
     custom = [
         path('top-commenter/', self.admin_view(self.top_commenter_view), name='top_commenter'),
+        path('top-movie/', self.admin_view(self.top_movie_view), name='top_movie'),
     ]
     return custom + original_get_urls(self)
 
 AdminSite.get_urls = patched_get_urls
 AdminSite.top_commenter_view = TopCommenterAdmin.top_commenter_view
+AdminSite.top_movie_view = TopMovieAdmin.top_movie_view
 
         
         
